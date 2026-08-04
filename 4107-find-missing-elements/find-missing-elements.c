5fe51c255a9b1f86ce65b1c0b723f78ce885e09f
Jishnu Mondal <121307106+Jishnu09-siuu@users.noreply.github.com>
2026-08-04 21:40:12 +0530

    Time: 0 ms (100.00%) | Memory: 13.4 MB (13.59%) - LeetSync

---
+++ 4107-find-missing-elements/find-missing-elements.c
@@ -0,0 +1,28 @@
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* findMissingElements(int* nums, int numsSize, int* returnSize) {
+    int min = nums[0] , max = nums[0];
+    for(int i = 0;i<numsSize;i++){
+        if(nums[i]<min){
+            min = nums[i];
+        }
+        if(nums[i]>max){
+            max = nums[i];
+        }
+    }
+    int present[101] = {0};
+    for(int i = 0;i<numsSize;i++){
+        present[nums[i]] = 1;
+    }
+    int *ans = (int *)malloc((max-min+1)*sizeof(int));
+    int k = 0;
+    for(int i = min ;i<=max ; i++){
+        if(!present[i]){
+            ans[k++] = i;
+        }
+    }
+    *returnSize = k;
+    return ans;
+    
+}
